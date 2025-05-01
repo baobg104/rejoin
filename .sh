9a8ccda2f656d101cfda07aaae6e154700e01638
baobg104 <loczutakii44@gmail.com>
2025-05-01 09:55:37 +0700

    Update .sh

--- .sh
+++ .sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 termux-setup-storage && yes | pkg update && yes | pkg upgrade && yes | pkg i python && yes | pkg install python-pip && pkg install python tsu libexpat openssl -y && pip install requests Flask colorama aiohttp psutil crypto pycryptodome loguru prettytable 
-curl -Ls "" -o /sdcard/Download/sun-Rejoin.py
+curl -Ls "https://raw.githubusercontent.com/baobg104/rejoin/refs/heads/main/wrejoin" -o /sdcard/Download/sun-Rejoin.py
